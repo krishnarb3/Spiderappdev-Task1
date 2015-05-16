9f9551e057172ea30cf60deebd09a41caa2b952b
krishnarb3 <krishnarb3@gmail.com>
2015-05-16 11:30:19 +0530

    Initial commit

---
+++ app/src/main/java/com/spiderinductions/rb/task1/MainActivity.java
@@ -0,0 +1,54 @@
+package com.spiderinductions.rb.task1;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends ActionBarActivity {
+    private Button button1;
+    private TextView textview1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button1=(Button)findViewById(R.id.button1);
+        textview1=(TextView)findViewById(R.id.textview1);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            textview1.setTextAlignment(View.TEXT_ALIGNMENT_CENTER);
+            textview1.setText("Hello World");
+
+            }
+        });
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
